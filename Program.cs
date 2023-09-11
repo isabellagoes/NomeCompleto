bdd0d1b90a877e5ab4aa7797ff94e7712980ab65
Isabella Goes <isabellagoes.igs@gmail.com>
2023-09-10 21:47:17 -0300

    Exibicao de nome e sobrenome

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Insira a seguir:");
+Console.WriteLine();
+
+Console.Write("Seu nome: ");
+string nome;
+nome = Console.ReadLine()!;
+
+Console.Write("Seu sobrenome: ");
+string sobrenome;
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
